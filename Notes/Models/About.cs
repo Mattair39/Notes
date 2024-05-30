011f7249e9c6d476e0c9200d3062b6774e3f7e37
Mattair39 <105869714+Mattair39@users.noreply.github.com>
2024-05-30 07:16:41 -0500

    Add project files.

---
+++ Notes/Models/About.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Models;
+
+internal class About
+{
+    public string Nombre => "Gabriel Arguello";
+    public string Descripcion => "Estudiante de 5to semestre de Ingeniería de Software, apasionado de las matemáticas y de los deportes. Entre sus intereses se destaca el entrenar pádel, jugar ajedrez e investigar constantemente sobre nuevos hitos en el espacio tecnológico.  ";
+}
